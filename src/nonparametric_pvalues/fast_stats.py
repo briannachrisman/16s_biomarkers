Cleaning up, fixing and adding methods for lefse, machine learning, FDR computation

src/nonparametric_pvalues/fast_stats.py
@@ -2,16 +2,17 @@ import numpy as np
 from scipy.stats import *
 
 
-def countLessThanEqual(pvals_data, pvals_permute):
+def countGreaterThanEqual(pvals_data, pvals_permute):
     idx_data = 0
     idx_permute = 0
-    pvals_count = np.zeros(len(pvals_data))
+    L = len(pvals_permute)
+    pvals_count = np.zeros(len(pvals_data)) 
     while (idx_permute<len(pvals_permute)) & (idx_data<len(pvals_data)):
-        if pvals_permute[idx_permute]<=pvals_data[idx_data]:
-            pvals_count[idx_data:] = pvals_count[idx_data:] + 1
-            idx_permute = idx_permute + 1
-        else:
+        if pvals_permute[idx_permute]>=pvals_data[idx_data]:
+            pvals_count[idx_data] = L-idx_permute
             idx_data = idx_data + 1
+        else:
+            idx_permute = idx_permute + 1
     return pvals_count
 
 def mannwhitneyu_fast(x, y):
@@ -22,11 +23,11 @@ def mannwhitneyu_fast(x, y):
     u2 = n1*n2 - u1  # remainder is U for y
     T = tiecorrect(np.concatenate([x,y]))
     if T == 0:
-        raise ValueError('All numbers are identical in mannwhitneyu')
+        return 0
     sd = np.sqrt(T * n1 * n2 * (n1+n2+1) / 12.0)
     meanrank = n1*n2/2.0 + 0.5
     bigu = max(u1, u2)
-    z = (bigu - meanrank) / sd
+    z = (1*(u1>u2) - 1*(u2>=u1))*(bigu - meanrank) / sd
     return z
     
 def wilcoxon_fast(diffs_gt_zero, diffs_rank):
@@ -36,4 +37,4 @@ def wilcoxon_fast(diffs_gt_zero, diffs_rank):
     W = sum(diffs_rank*diffs_gt_zero)
     se = np.sqrt(count*(count+1)*(2*count+1)/6)
     z = W/se
-    return abs(z)
+    return z

src/nonparametric_pvalues/fdr.py
@@ -13,45 +13,54 @@ from fast_stats import *
 
 dataset = sys.argv[1]
 biomarker = sys.argv[2]
-PERMUTE_START = int(sys.argv[3])
-PERMUTE_END = int(sys.argv[4])
+SEED_IDX = int(sys.argv[3])
 
 BIOMARKER_DIR = '/home/groups/dpwall/briannac/sequence_based_biomarkers/'
 
 
-pvals_data = sorted(np.load(BIOMARKER_DIR + 'results/pvalues/pvals_%s_%s.npy' % (dataset, biomarker)))
-permuted_phenos = np.load(BIOMARKER_DIR + 'intermediate_files/permutation_test/%s_phenos_permuted.npy' % dataset).astype(int)[PERMUTE_START:PERMUTE_END]
-pvals_count = np.zeros(len(pvals_data)).astype(int)
-step_size=100000
-n_iters=len(permuted_phenos)
-person_biomarker = scipy.sparse.load_npz(BIOMARKER_DIR + 'results/generate_biomarkers/sample_vs_biomarker_%s_%s.npz' % (biomarker, dataset))
+pvals_data = sorted(np.load(BIOMARKER_DIR + 'results/rank_stats/rank_stat_%s_%s.npy' % (dataset, biomarker)))
+permuted_phenos = np.load(BIOMARKER_DIR + 'intermediate_files/permutation_test/%s_phenos_permuted.npy' % dataset).astype(int)
+#permuted_phenos[:,::2] = 0
+#permuted_phenos[:,1::2] = 1
 
+pvals_count = np.zeros(len(pvals_data)).astype(int)
+biomarker_sample_size = 100000
+n_biomarker_samples = 10
+n_iters=1
+person_biomarker = scipy.sparse.load_npz(BIOMARKER_DIR + 'results/generate_biomarkers/sample_vs_biomarker_%s_%s.npz' % (biomarker, dataset))
+print(np.shape(person_biomarker))
+pvals_total = []
 t = time.time()
-print('person x biomarker shape: ', np.shape(person_biomarker))
-for chunk_start in tqdm(np.arange(0,np.shape(person_biomarker)[1], step=step_size)):
-    chunk_end = min(chunk_start+step_size, np.shape(person_biomarker)[1])
-    person_biomarker_current = person_biomarker[:,chunk_start:chunk_end].toarray()
+for i_biomarker_sample in range(n_biomarker_samples):
+    print(i_biomarker_sample)
+    np.random.seed(i_biomarker_sample + 10000*SEED_IDX)
+    person_biomarker_current = person_biomarker[:,np.random.choice([i for i in range(np.shape(person_biomarker)[1])], biomarker_sample_size, replace=True)].toarray()
     if 'autism' in dataset:
         d = person_biomarker_current[::2]-person_biomarker_current[1::2]
         diffs_gt_zeros = 1*(d>0) - 1*(d<0)
         d[d==0] = np.nan
-        diffs_rank = mstats.rankdata(np.ma.masked_invalid(d), axis=1)
+        diffs_rank = mstats.rankdata(abs(np.ma.masked_invalid(d)), axis=0)
         diffs_rank = diffs_rank.transpose()
         diffs_gt_zeros = diffs_gt_zeros.transpose()
-#        d = (diffs_gt_zero-diffs_lt_zero).transpose()
     if 'obesity' in dataset:
         ranks = rankdata(person_biomarker_current, axis=0)
-    for i in range(n_iters):
+    for i in range(n_iters): # MAKE RAND_IDX A CHOICE
+        rand_idx=np.random.choice([i for i in range(np.shape(person_biomarker_current)[1])])
         if 'autism' in dataset:
-            diffs = np.apply_along_axis(arr=d, axis=0,func1d=lambda x: x*permuted_phenos[i,::2])
-            pvals_permute = [wilcoxon_fast(rank, diffs_gt_zero) for diffs_gt_zero, rank in zip(diffs_gt_zeros, diffs_rank)]
+            diffs_gt_zero_permute = diffs_gt_zeros*(2*permuted_phenos[rand_idx,::2]-1)
+            pvals_permute = [wilcoxon_fast(diffs_gt_zero, rank) for diffs_gt_zero, rank in zip(diffs_gt_zero_permute, diffs_rank)]
         elif 'obesity' in dataset:
-            affected = ranks[np.where(permuted_phenos[i])[0],:]
-            unaffected = ranks[np.where(np.invert(permuted_phenos[i]))[0],:]
+            affected = ranks[np.where(permuted_phenos[rand_idx])[0],:]
+            unaffected = ranks[np.where(permuted_phenos[rand_idx]==False)[0],:]
             pvals_permute = sorted([mannwhitneyu_fast(
                 affected[:,i],
                 unaffected[:,i]) for i in range(np.shape(person_biomarker_current)[1])])
-        pvals_count = pvals_count + countLessThanEqual(pvals_data, pvals_permute)
+        #pvals_count = pvals_count + countGreaterThanEqual(pvals_data, pvals_permute)/len(pvals_permute)/n_iters/n_biomarker_samples]
+        pvals_total = pvals_total + list(pvals_permute)
+print('max pval permute: ', max(pvals_total))
+
 #fdr = [p/(i+.0001)/n_iters for i,p in enumerate(pvals_count)]
-np.save(BIOMARKER_DIR + 'intermediate_files/nonparametric_pvalues/%s_%s/false_discovery_count_%s_%s_%i.npy' % (dataset, biomarker, dataset, biomarker, PERMUTE_START), pvals_count)
-print('saved at ', BIOMARKER_DIR + 'intermediate_files/nonparametric_pvalues/%s_%s/false_discovery_count_%s_%s_%i.npy' % (dataset, biomarker, dataset, biomarker, PERMUTE_START))
+#np.save(BIOMARKER_DIR + 'intermediate_files/nonparametric_pvalues/%s_%s/false_discovery_count_%s_%s_%i.npy' % (dataset, biomarker, dataset, biomarker, SEED_IDX), pvals_count)
+np.savetxt(BIOMARKER_DIR + 'intermediate_files/nonparametric_pvalues/%s_%s/permuted_rank_stat_%s_%s_%i.txt' % (dataset, biomarker, dataset, biomarker, SEED_IDX), pvals_total)
+
+print('saved at ', BIOMARKER_DIR + 'intermediate_files/nonparametric_pvalues/%s_%s/false_discovery_count_%s_%s_%i.npy' % (dataset, biomarker, dataset, biomarker, SEED_IDX))

src/nonparametric_pvalues/rank_stat.py
@@ -31,10 +31,9 @@ for chunk_start in tqdm(np.arange(0,np.shape(person_biomarker)[1], step=step_siz
         d = person_biomarker_current[::2]-person_biomarker_current[1::2]
         diffs_gt_zeros = 1*(d>0) - 1*(d<0)
         d[d==0] = np.nan
-        diffs_rank = mstats.rankdata(np.ma.masked_invalid(d), axis=1)
+        diffs_rank = mstats.rankdata(abs(np.ma.masked_invalid(d)), axis=0)
         diffs_rank = diffs_rank.transpose()
         diffs_gt_zeros = diffs_gt_zeros.transpose()
-        
         pvalues[chunk_start:chunk_end] = [wilcoxon_fast(d, rank) for d, rank in zip(diffs_gt_zeros, diffs_rank)]
     if 'obesity' in dataset:
         ranks = rankdata(person_biomarker_current, axis=0)
@@ -43,5 +42,5 @@ for chunk_start in tqdm(np.arange(0,np.shape(person_biomarker)[1], step=step_siz
         pvalues[chunk_start:chunk_end] = [mannwhitneyu_fast(
                 affected[:,i],
                 unaffected[:,i]) for i in range(np.shape(person_biomarker_current)[1])]
-np.save(BIOMARKER_DIR + 'results/pvalues/pvals_%s_%s.npy' % (dataset, biomarker), pvalues)
+np.save(BIOMARKER_DIR + 'results/rank_stats/rank_stat_%s_%s.npy' % (dataset, biomarker), pvalues)
 print('success -- min stat', min(pvalues), ' max stat: ', max(pvalues))